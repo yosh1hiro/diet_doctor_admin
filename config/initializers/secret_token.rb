a828efa4533f1a698294af6e7bdc875a8c1eca32
kazhida <kazhida@abplus.com>
2014-04-06 17:30:34 +0900

    Railsアプリとして仕切り直し

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DietDoctorAdmin::Application.config.secret_key_base = '62d356d7f31f19eb27c612b749f5c14f60b95ceea048e303f2a258f0716c64cce2a8f41d6a58b16a9161d9fd259382ef11ab16338bb1c98800a266316b0bbd9f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DietDoctorAdmin::Application.config.session_store :cookie_store, key: '_diet_doctor_admin_session'
